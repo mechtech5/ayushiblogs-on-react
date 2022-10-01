8834534109a84cd139c4045bbe726ca5794e6c4f
Ayush Likhar <neontechhead@gmail.com>
2022-10-01 19:27:28 +0530

    init

---
+++ src/components/Blog.tsx
@@ -0,0 +1,17 @@
+import BlogList from "./BlogList";
+import IBlog from "./service/interface";
+import { BlogData } from "./service/BlogData";
+
+export default function Blog() {
+  return (
+    <div className="container">
+      <div className="content">
+        <ul>
+          {BlogData.map((data: IBlog) => (
+            <BlogList key={data.id} data={data} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
